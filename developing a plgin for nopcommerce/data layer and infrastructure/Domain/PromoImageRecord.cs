0f6f3df7c8402005db99d4055c8e7949fa9e567b
aly ba <alybas2002@yahoo.fr>
2015-03-02 17:34:05 +0100

    nopcommerce module data layer

---
+++ developing a plgin for nopcommerce/data layer and infrastructure/Domain/PromoImageRecord.cs
@@ -0,0 +1,20 @@
+﻿using Nop.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nop.Plugin.Widget.PromoSlider.Domain
+{
+    public class PromoImageRecord : BaseEntity
+    {
+        public virtual int PromoImageId { get; set; }
+        public virtual int PromoSliderId { get; set; }
+        public virtual string Caption { get; set; }
+        public virtual string Url { get; set; }
+        public virtual string FilePath { get; set; }
+        public virtual int DisplayOrder { get; set; }
+
+        public PromoSliderRecord PromoSlider { get; set; }
+    }
+}
